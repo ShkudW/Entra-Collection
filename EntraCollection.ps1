6da67447c5ac0feb58ff17381503eb630ca3c3c4
Shaked Wiessman <shakedwe2@gmail.com>
2025-05-21 14:51:11 +0300

    Update EntraCollection.ps1

--- EntraCollection.ps1
+++ EntraCollection.ps1
@@ -14,35 +14,34 @@ function Get-Tokens {
 		
 		#>	
 
-	    param(
+		param(
 		[Parameter(Mandatory = $false)]
-        [string]$DomainName,
-	    [Parameter(Mandatory = $false)]
-        [switch]$Graph,
-	    [Parameter(Mandatory = $false)]
-        [switch]$ARM		
+		[string]$DomainName,
+		[Parameter(Mandatory = $false)]
+		[switch]$Graph,
+		[Parameter(Mandatory = $false)]
+		[switch]$ARM		
 		)
 		
 		
-				if ($DomainName -and -not $Graph -and -not $ARM){
-					Write-Host "[!] Please choose between Graph Token or ARM Token" -ForegroundColor Red
-					Write-Host "Usage: Get-Tokens -DomainName domain.com -Graph | -ARM"
-					return
-				}
+			if ($DomainName -and -not $Graph -and -not $ARM){
+				Write-Host "[!] Please choose between Graph Token or ARM Token" -ForegroundColor Red
+				Write-Host "Usage: Get-Tokens -DomainName domain.com -Graph | -ARM"
+				return
+			}
 
-				if (-not $DomainName -and -not $Graph -and -not $ARM){
-					Write-Host "[!] Please provide -DomainName and select -Graph or -ARM" -ForegroundColor Red
-					Write-Host "Usage: Get-Tokens -DomainName domain.com -Graph | -ARM"
-					return
-				}
+			if (-not $DomainName -and -not $Graph -and -not $ARM){
+				Write-Host "[!] Please provide -DomainName and select -Graph or -ARM" -ForegroundColor Red
+				Write-Host "Usage: Get-Tokens -DomainName domain.com -Graph | -ARM"
+				return
+			}
 				
-				if ($Graph -and $ARM) {
-					Write-Host "[!] Please select only one API: either -Graph or -ARM, not both." -ForegroundColor Red
-					return
-				}
+			if ($Graph -and $ARM) {
+				Write-Host "[!] Please select only one API: either -Graph or -ARM, not both." -ForegroundColor Red
+				return
+			}
 
 
-			
 			function Get-DomainName {
 				try {
 					$response = Invoke-RestMethod -Method GET -Uri "https://login.microsoftonline.com/$DomainName/.well-known/openid-configuration"
@@ -50,9 +49,9 @@ function Get-Tokens {
 					Write-Host "[*] Tenant ID for $DomainName is $TenantID" -ForegroundColor DarkCyan
 					return $TenantID
 				} catch {
-						Write-Error "[-] Failed to retrieve Tenant ID from domain: $DomainName"
-						return $null
-					}
+					Write-Error "[-] Failed to retrieve Tenant ID from domain: $DomainName"
+					return $null
+				}
 			}
 			
 			$TenantID = Get-DomainName
@@ -72,141 +71,139 @@ function Get-Tokens {
 					"scope"     = "https://management.azure.com/.default"
 				}
 			
-			if($Graph) {
-				$authResponse = Invoke-RestMethod -Method POST -Uri $deviceCodeUrl -Headers $headers -Body $GraphBody
-				$code = $authResponse.user_code
-				$deviceCode = $authResponse.device_code
+				if($Graph) {
+						$authResponse = Invoke-RestMethod -Method POST -Uri $deviceCodeUrl -Headers $headers -Body $GraphBody
+						$code = $authResponse.user_code
+						$deviceCode = $authResponse.device_code
 
-				Write-Host "`n[*] Browser will open in 5 sec, Please enter this code:" -ForegroundColor DarkCyan -NoNewline
-				Write-Host " $code" -ForegroundColor DarkYellow
-				Start-Sleep -Seconds 5
-				Start-Process "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" -ArgumentList "https://microsoft.com/devicelogin"
+						Write-Host "`n[*] Browser will open in 5 sec, Please enter this code:" -ForegroundColor DarkCyan -NoNewline
+						Write-Host " $code" -ForegroundColor DarkYellow
+						Start-Sleep -Seconds 5
+						Start-Process "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" -ArgumentList "https://microsoft.com/devicelogin"
 
-				$tokenUrl = "https://login.microsoftonline.com/$TenantID/oauth2/token?api-version=1.0"
-				$tokenBody = @{
-					"scope"      = "openid"
-					"client_id"  = "d3590ed6-52b3-4102-aeff-aad2292ab01c"
-					"grant_type" = "urn:ietf:params:oauth:grant-type:device_code"
-					"code"       = $deviceCode
+						$tokenUrl = "https://login.microsoftonline.com/$TenantID/oauth2/token?api-version=1.0"
+						$tokenBody = @{
+							"scope"      = "openid"
+							"client_id"  = "d3590ed6-52b3-4102-aeff-aad2292ab01c"
+							"grant_type" = "urn:ietf:params:oauth:grant-type:device_code"
+							"code"       = $deviceCode
+						}
+				
+					while ($true) {
+						try {
+						   
+							$tokenResponse = Invoke-RestMethod -Method POST -Uri $tokenUrl -Headers $headers -Body $tokenBody -ErrorAction Stop
+							$RefreshToken = $tokenResponse.refresh_token
+
+							
+							Set-Content -Path "C:\Users\Public\Refreshtoken.txt" -Value $RefreshToken
+							Write-Host "`n[+] Refresh Token saved to C:\Users\Public\Refreshtoken.txt" -ForegroundColor Cyan
+
+							
+							$url = "https://login.microsoftonline.com/$TenantID/oauth2/v2.0/token?api-version=1.0"
+							$refreshBody = @{
+								"client_id"     = "d3590ed6-52b3-4102-aeff-aad2292ab01c"
+								"scope"         = "https://graph.microsoft.com/.default"
+								"grant_type"    = "refresh_token"
+								"refresh_token" = $RefreshToken
+							}
+
+							try {
+								$refreshResponse = Invoke-RestMethod -Method POST -Uri $url -Body $refreshBody -ContentType "application/x-www-form-urlencoded"
+								$AccessToken = $refreshResponse.access_token
+								Write-Host "`n[+] Access Token retrieved." -ForegroundColor Green
+							} catch {
+								Write-Host "`n[-] Failed to retrieve Access Token using Refresh Token." -ForegroundColor Red
+								return $null
+							}
+
+						   
+							$Global:AccessToken  = $AccessToken
+							$Global:RefreshToken = $RefreshToken
+
+							Write-Host "`n[+] Access Token stored in variable:`n$AccessToken" -ForegroundColor Green
+							return
+						} catch {
+							$errorResponse = $_.ErrorDetails.Message | ConvertFrom-Json
+							if ($errorResponse.error -eq "authorization_pending") {
+								Start-Sleep -Seconds 5
+							} elseif ($errorResponse.error -eq "authorization_declined" -or $errorResponse.error -eq "expired_token") {
+								Write-Host "`n[-] Authorization failed or expired." -ForegroundColor Red
+								return
+							} else {
+								Write-Host "`n[-] Unexpected error: $($errorResponse.error)" -ForegroundColor Red
+								return
+							}
+						}
+					}
+			
+				}else {
+			
+					$authResponse = Invoke-RestMethod -Method POST -Uri $deviceCodeUrl -Headers $headers -Body $ARMBody
+					$code = $authResponse.user_code
+					$deviceCode = $authResponse.device_code
+
+					Write-Host "`n[*] Browser will open in 5 sec, Please enter this code:" -ForegroundColor DarkCyan -NoNewline
+					Write-Host " $code" -ForegroundColor DarkYellow
+					Start-Sleep -Seconds 5
+					Start-Process "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" -ArgumentList "https://microsoft.com/devicelogin"
+
+					$tokenUrl = "https://login.microsoftonline.com/$TenantID/oauth2/token?api-version=1.0"
+					$tokenBody = @{
+						"scope"      = "openid"
+						"client_id"  = "d3590ed6-52b3-4102-aeff-aad2292ab01c"
+						"grant_type" = "urn:ietf:params:oauth:grant-type:device_code"
+						"code"       = $deviceCode
+					}
+
+					while ($true) {
+						try {
+						   
+								$tokenResponse = Invoke-RestMethod -Method POST -Uri $tokenUrl -Headers $headers -Body $tokenBody -ErrorAction Stop
+								$RefreshToken = $tokenResponse.refresh_token
+
+								Set-Content -Path "C:\Users\Public\Refreshtoken.txt" -Value $RefreshToken
+								Write-Host "`n[+] Refresh Token saved to C:\Users\Public\Refreshtoken.txt" -ForegroundColor Cyan
+							
+								$url = "https://login.microsoftonline.com/$TenantID/oauth2/v2.0/token?api-version=1.0"
+								$refreshBody = @{
+									"client_id"     = "d3590ed6-52b3-4102-aeff-aad2292ab01c"
+									"scope"         = "https://management.azure.com/.default"
+									"grant_type"    = "refresh_token"
+									"refresh_token" = $RefreshToken
+								}
+
+							try {
+								$refreshResponse = Invoke-RestMethod -Method POST -Uri $url -Body $refreshBody -ContentType "application/x-www-form-urlencoded"
+								$AccessToken = $refreshResponse.access_token
+								Write-Host "`n[+] Access Token retrieved." -ForegroundColor Green
+							} catch {
+								Write-Host "`n[-] Failed to retrieve Access Token using Refresh Token." -ForegroundColor Red
+								return $null
+							}
+
+							$Global:AccessToken  = $AccessToken
+							$Global:RefreshToken = $RefreshToken
+
+							Write-Host "`n[+] Access Token stored in variable:`n$AccessToken" -ForegroundColor Green
+							return
+							
+						} catch {
+							$errorResponse = $_.ErrorDetails.Message | ConvertFrom-Json
+							if ($errorResponse.error -eq "authorization_pending") {
+								Start-Sleep -Seconds 5
+							} elseif ($errorResponse.error -eq "authorization_declined" -or $errorResponse.error -eq "expired_token") {
+								Write-Host "`n[-] Authorization failed or expired." -ForegroundColor Red
+								return
+							} else {
+								Write-Host "`n[-] Unexpected error: $($errorResponse.error)" -ForegroundColor Red
+								return
+							}
+						}
+					}
+				
 				}
 				
-				while ($true) {
-					try {
-					   
-						$tokenResponse = Invoke-RestMethod -Method POST -Uri $tokenUrl -Headers $headers -Body $tokenBody -ErrorAction Stop
-						$RefreshToken = $tokenResponse.refresh_token
-
-						
-						Set-Content -Path "C:\Users\Public\Refreshtoken.txt" -Value $RefreshToken
-						Write-Host "`n[+] Refresh Token saved to C:\Users\Public\Refreshtoken.txt" -ForegroundColor Cyan
-
-						
-						$url = "https://login.microsoftonline.com/$TenantID/oauth2/v2.0/token?api-version=1.0"
-						$refreshBody = @{
-							"client_id"     = "d3590ed6-52b3-4102-aeff-aad2292ab01c"
-							"scope"         = "https://graph.microsoft.com/.default"
-							"grant_type"    = "refresh_token"
-							"refresh_token" = $RefreshToken
-						}
-
-						try {
-							$refreshResponse = Invoke-RestMethod -Method POST -Uri $url -Body $refreshBody -ContentType "application/x-www-form-urlencoded"
-							$AccessToken = $refreshResponse.access_token
-							Write-Host "`n[+] Access Token retrieved." -ForegroundColor Green
-						} catch {
-							Write-Host "`n[-] Failed to retrieve Access Token using Refresh Token." -ForegroundColor Red
-							return $null
-						}
-
-					   
-						$Global:AccessToken  = $AccessToken
-						$Global:RefreshToken = $RefreshToken
-
-						Write-Host "`n[+] Access Token stored in variable:`n$AccessToken" -ForegroundColor Green
-						return
-					} catch {
-						$errorResponse = $_.ErrorDetails.Message | ConvertFrom-Json
-						if ($errorResponse.error -eq "authorization_pending") {
-							Start-Sleep -Seconds 5
-						} elseif ($errorResponse.error -eq "authorization_declined" -or $errorResponse.error -eq "expired_token") {
-							Write-Host "`n[-] Authorization failed or expired." -ForegroundColor Red
-							return
-						} else {
-							Write-Host "`n[-] Unexpected error: $($errorResponse.error)" -ForegroundColor Red
-							return
-						}
-					}
-				}
-			
-			}else {
-			
-				$authResponse = Invoke-RestMethod -Method POST -Uri $deviceCodeUrl -Headers $headers -Body $ARMBody
-				$code = $authResponse.user_code
-				$deviceCode = $authResponse.device_code
-
-				Write-Host "`n[*] Browser will open in 5 sec, Please enter this code:" -ForegroundColor DarkCyan -NoNewline
-				Write-Host " $code" -ForegroundColor DarkYellow
-				Start-Sleep -Seconds 5
-				Start-Process "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" -ArgumentList "https://microsoft.com/devicelogin"
-
-				$tokenUrl = "https://login.microsoftonline.com/$TenantID/oauth2/token?api-version=1.0"
-				$tokenBody = @{
-					"scope"      = "openid"
-					"client_id"  = "d3590ed6-52b3-4102-aeff-aad2292ab01c"
-					"grant_type" = "urn:ietf:params:oauth:grant-type:device_code"
-					"code"       = $deviceCode
-				}
-
-				while ($true) {
-				try {
-				   
-					$tokenResponse = Invoke-RestMethod -Method POST -Uri $tokenUrl -Headers $headers -Body $tokenBody -ErrorAction Stop
-					$RefreshToken = $tokenResponse.refresh_token
-
-					
-					Set-Content -Path "C:\Users\Public\Refreshtoken.txt" -Value $RefreshToken
-					Write-Host "`n[+] Refresh Token saved to C:\Users\Public\Refreshtoken.txt" -ForegroundColor Cyan
-
-					
-					$url = "https://login.microsoftonline.com/$TenantID/oauth2/v2.0/token?api-version=1.0"
-					$refreshBody = @{
-						"client_id"     = "d3590ed6-52b3-4102-aeff-aad2292ab01c"
-						"scope"         = "https://management.azure.com/.default"
-						"grant_type"    = "refresh_token"
-						"refresh_token" = $RefreshToken
-					}
-
-					try {
-						$refreshResponse = Invoke-RestMethod -Method POST -Uri $url -Body $refreshBody -ContentType "application/x-www-form-urlencoded"
-						$AccessToken = $refreshResponse.access_token
-						Write-Host "`n[+] Access Token retrieved." -ForegroundColor Green
-					} catch {
-						Write-Host "`n[-] Failed to retrieve Access Token using Refresh Token." -ForegroundColor Red
-						return $null
-					}
-
-				   
-					$Global:AccessToken  = $AccessToken
-					$Global:RefreshToken = $RefreshToken
-
-					Write-Host "`n[+] Access Token stored in variable:`n$AccessToken" -ForegroundColor Green
-					return
-					
-				} catch {
-					$errorResponse = $_.ErrorDetails.Message | ConvertFrom-Json
-					if ($errorResponse.error -eq "authorization_pending") {
-						Start-Sleep -Seconds 5
-					} elseif ($errorResponse.error -eq "authorization_declined" -or $errorResponse.error -eq "expired_token") {
-						Write-Host "`n[-] Authorization failed or expired." -ForegroundColor Red
-						return
-					} else {
-						Write-Host "`n[-] Unexpected error: $($errorResponse.error)" -ForegroundColor Red
-						return
-					}
-				}
-			}
-			
-	}
 }
 
 
